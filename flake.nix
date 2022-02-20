e727f1db8f780fd61778de45770f13bf574d3103
Daniel Nagy <danielnagy@posteo.de>
2022-02-20 23:16:59 +0100

    Add hmmodule-zathura

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     lib.module-x86_64-linux = import ./module-x86_64-linux.nix;
     lib.hmmodule-mpv = import ./hmmodule-mpv.nix;
     lib.hmmodule-firefox = import ./module-firefox.nix;
+    lib.hmmodule-zathura = import ./hmmodule-zathura.nix;
 
   };
 }
---
+++ hmmodule-zathura.nix
@@ -0,0 +1,24 @@
+{ pkgs, font, fontSize, ... }:
+
+{
+  programs.zathura = {
+    options = {
+      font = "${font} ${toString fontSize}";
+      render-loading = false;
+      dbus-raise-window = false;
+      database = "null";
+      first-page-column = "1";
+      # recolor-reverse-video = true; # this prevents image pdfs from being recolored
+      # sandbox = "strict"; # this is broken. with this zathura does not show a window
+    };
+    # for extraconfig:
+    #    this is so you can open links in your browser. otherwise seccomp is active
+    #    set sandbox none
+    extraConfig = ''
+      map S feedkeys ":set first-page-column 1"<Return>
+      map D feedkeys ":set first-page-column 2"<Return>
+      map Q quit
+    '';
+  };
+
+}
