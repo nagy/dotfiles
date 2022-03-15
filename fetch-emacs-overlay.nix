a4ac01dfc3d3f17005d406ed1ece8d9342a9d004
Daniel Nagy <danielnagy@posteo.de>
2022-03-15 13:23:42 +0100

    Update emacs-overlay

--- fetch-emacs-overlay.nix
+++ fetch-emacs-overlay.nix
@@ -3,6 +3,6 @@ pkgs:
 pkgs.fetchFromGitHub {
   owner = "nix-community";
   repo = "emacs-overlay";
-  rev = "ac56c95c30b8a1df8eae040d1c132c63ca002a69";
-  sha256 = "11j3fq55acwkvza54hv9rld0k41j07v4qyq11k5cg4sk1rkm2z3h";
+  rev = "d43b200ed5675bf912440975435dd7a7363f9481";
+  sha256 = "11vwpf2kgwpsy3hw4f02mm93cn8mrkd4b5irkidvfvvxwlvw5zrv";
 }
