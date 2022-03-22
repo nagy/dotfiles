c4ea0a640b298c7adc439d69dd2d014bdf0aa13f
Daniel Nagy <danielnagy@posteo.de>
2022-03-22 15:00:30 +0100

    Convert lib to also take pkgs
    This is needed because most of the functions depend on it.

--- flake.nix
+++ flake.nix
@@ -3,25 +3,26 @@
 
   outputs = { self }: {
 
-    lib = {
-      module-common = import ./module-common.nix;
-      module-x86_64-linux = import ./module-x86_64-linux.nix;
-      hmmodule-mpv = import ./hmmodule-mpv.nix;
-      hmmodule-firefox = import ./module-firefox.nix;
-      hmmodule-zathura = import ./hmmodule-zathura.nix;
-      hmmodule-alacritty-day = import ./hmmodule-alacritty-day.nix;
-      hmmodule-alacritty-night = import ./hmmodule-alacritty-night.nix;
+    lib = { pkgs, lib ? pkgs.lib }:
+      ({
+        module-common = import ./module-common.nix;
+        module-x86_64-linux = import ./module-x86_64-linux.nix;
+        hmmodule-mpv = import ./hmmodule-mpv.nix;
+        hmmodule-firefox = import ./module-firefox.nix;
+        hmmodule-zathura = import ./hmmodule-zathura.nix;
+        hmmodule-alacritty-day = import ./hmmodule-alacritty-day.nix;
+        hmmodule-alacritty-night = import ./hmmodule-alacritty-night.nix;
 
-      fetch-home-manager = import ./fetch-home-manager.nix;
-      fetch-emacs-overlay = import ./fetch-emacs-overlay.nix;
+        fetch-home-manager = import ./fetch-home-manager.nix;
+        fetch-emacs-overlay = import ./fetch-emacs-overlay.nix;
 
-      conv-hmzathura2nixos = import ./conv-hmzathura2nixos.nix;
-      conv-hmmpv2nixos = import ./conv-hmmpv2nixos.nix;
+        conv-hmzathura2nixos = import ./conv-hmzathura2nixos.nix;
+        conv-hmmpv2nixos = import ./conv-hmmpv2nixos.nix;
 
-      pkg-journal-git-store = pkgs:
-        pkgs.writeScriptBin "journal-git-store"
-        (builtins.readFile ./bin/journal-git-store);
-    } // import ./lib.nix;
+        pkg-journal-git-store = pkgs:
+          pkgs.writeScriptBin "journal-git-store"
+          (builtins.readFile ./bin/journal-git-store);
+      } // (import ./lib.nix { inherit pkgs; }));
 
   };
 }
