ad8107aed24613bd1b13ef76d5687b1047bbf2dd
Daniel Nagy <danielnagy@posteo.de>
2022-03-23 23:43:04 +0100

    Update emacs-overlay and home-manager

--- fetch-emacs-overlay.nix
+++ fetch-emacs-overlay.nix
@@ -3,6 +3,6 @@ pkgs:
 pkgs.fetchFromGitHub {
   owner = "nix-community";
   repo = "emacs-overlay";
-  rev = "ccb4f7c6af0b2dc5c02aec6f28d5cffb199087c4";
-  sha256 = "1sg87dskck70kglm72vydz0f2aqrvnill5a9lxb0k75yjw008yxb";
+  rev = "f592174794d3bc90df3b83671541dae2642c23ff";
+  sha256 = "1v7rg1s9qwpwhmd37ainwk6klhf4x7ifjg9h5amgss744gg4hin4";
 }
--- fetch-home-manager.nix
+++ fetch-home-manager.nix
@@ -3,6 +3,6 @@ pkgs:
 pkgs.fetchFromGitHub {
   owner = "nix-community";
   repo = "home-manager";
-  rev = "835797f3a4a59459a316ae8d4ab91fa59faf61a4";
-  sha256 = "0kkfs0jdm42r4a7jhb312qppchi8wk6vgh565cqyf079kvgih0il";
+  rev = "e2ebc3a3af4d6a942cea2d7d5fe46d56edcecc8b";
+  sha256 = "1kyr4dbp8z8q9a9k4dc7y22yzig2rwf6aayhkda2nag4c1pgwda9";
 }
