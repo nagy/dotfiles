9604515a3d6034e7f274b17e1521feb6593565c1
Daniel Nagy <danielnagy@posteo.de>
2022-05-29 13:33:02 +0200

    Add custom xkb keymap

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -19,11 +19,31 @@ in {
   # Configure keymap in X11
   services.xserver = {
     enable = true;
-    layout = "de";
-    # make the accents key input the character with only one press instead of two
-    xkbVariant = "nodeadkeys";
+    layout = "mine";
     # Configure X11 window manager
     displayManager.startx.enable = true;
+    extraLayouts = {
+      mine = {
+        description = "my custom xkb layout";
+        languages = [ "deu" ];
+        symbolsFile = (pkgs.writeText "myinclude.conf" ''
+          xkb_symbols "mine" {
+             include "pc"
+             include "de"
+             // key <MENU> { [  Hyper_R  ] };
+             key <RWIN> { [  Hyper_R  ] };
+             key <INS> { [ Multi_key, Multi_key, 8, 9  ] };
+             // modifier_map Mod3   { <MENU> };
+             modifier_map Mod3   { <RWIN> };
+             key <TAB>  { [ Tab,  ISO_Left_Tab,  6,  3 ] };
+             key <CAPS> { [ Escape, Escape, 4, 5  ] };
+             key <SPCE> { [space, space, circle, U232F ] };
+             // this overwrites dead_caron
+             key <AC11> { [adiaeresis, Adiaeresis, circle, U232F ] };
+          };
+        '');
+      };
+    };
     # logFile = "/dev/null"; # the default
     # windowManager.exwm.enable = true;
     videoDrivers = [ "amdgpu" ];
