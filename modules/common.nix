c36381a188e52793d0da383c1e929f5d802d2ccf
Daniel Nagy <danielnagy@posteo.de>
2022-06-16 21:58:16 +0200

    Add dot to NIX_PATH

--- modules/common.nix
+++ modules/common.nix
@@ -325,6 +325,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
+    nixPath = [ "dot=${../.}" ];
     registry = {
       nagy.to = {
         owner = "nagy";
