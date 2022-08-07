7151a411c8a5915dd8b1f4eb180c0bb872df8193
Daniel Nagy <danielnagy@posteo.de>
2022-08-07 21:00:02 +0200

    Fix fzf shell

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -4,9 +4,11 @@
   environment.systemPackages = [ pkgs.fzf ];
 
   programs.bash.interactiveShellInit = ''
-    if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
-      . ${pkgs.fzf}/share/fzf/completion.bash
-      . ${pkgs.fzf}/share/fzf/key-bindings.bash
+    if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "vterm") ]]; then
+      if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
+        . ${pkgs.fzf}/share/fzf/completion.bash
+        . ${pkgs.fzf}/share/fzf/key-bindings.bash
+      fi
     fi
   '';
 }
