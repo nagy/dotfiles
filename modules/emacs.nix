8207b0083c4286e736ee3536e4a4656577859606
Daniel Nagy <danielnagy@posteo.de>
2022-09-05 16:43:22 +0200

    Emacs packages cleanup
    Most have been moved to doom config

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,103 +1,18 @@
 { config, pkgs, ... }:
 
 let
-  systemEmacs = config.services.emacs.package;
-  customEmacsPackages =
-    systemEmacs.pkgs.overrideScope' (self: super: { emacs = systemEmacs; });
+  customEmacsPackages = pkgs.emacsPackagesFor config.services.emacs.package;
   emacsAndPackages = customEmacsPackages.withPackages (epkgs:
     (with epkgs;
       with epkgs.melpaPackages; [
-        use-package
-        general
         vterm
-        org
         pdf-tools
-        org-pdftools
-        org-bookmark-heading
-
+        # org-pdftools
         pass
-
-        evil
-
-        org-brain
-        ascii-art-to-unicode # for org-brain fancyness
-
-        org-superstar
-        org-ql
-        elfeed
-        dired-narrow
-        dired-subtree
-        dired-collapse
-        outshine
-        focus
-
-        flycheck
-
-        disk-usage
-
-        ts # timestamp library
-
-        imenu-list # somehow the doom provided one does not work
-
-        bufler
-
-        magit-section
-        magit
-        forge
-        transient
-        modus-themes
-        lin
-
-        exwm
-
-        # modeline
-        telephone-line
-        diminish
-
-        # ledger-mode
-        company-ledger
-
-        info-colors
-
-        # lisps
-        lispy # this pulls in ivy
-        lispyville
-        sly
-        sly-macrostep
-        sly-repl-ansi-color
-
-        # completion
-        vertico
-        embark
-        marginalia
-        orderless
-        consult
-        embark-consult
-
-        centered-cursor-mode
-
-        ov
-        treemacs
-        treemacs-all-the-icons
-        treemacs-evil
-        treemacs-magit
-        treemacs-projectile
-        s # string library
-
-        lsp-mode
-        reformatter
-        python-black
-        lsp-pyright
-
-        osm
-        helpful
-        yasnippet-snippets
-        arduino-mode
-        yaml-mode
-        gemini-mode
-        tokei
       ]));
 in {
-  environment.systemPackages =
-    [ emacsAndPackages (pkgs.mu.override { emacs = systemEmacs; }) ];
+  environment.systemPackages = [
+    emacsAndPackages
+    (pkgs.mu.override { inherit (customEmacsPackages) emacs; })
+  ];
 }
