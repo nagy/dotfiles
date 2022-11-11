ce9ee86e52b2a6576f8a24c60f52386377b74e05
Daniel Nagy <danielnagy@posteo.de>
2022-11-11 21:00:00 +0100

    Add journal-git-store module

---
+++ modules/journal-git-store.nix
@@ -0,0 +1,37 @@
+{ pkgs, lib, dot, ... }:
+
+with pkgs.callPackage dot.outputs.lib { }; {
+  systemd.services.journal-logger = {
+    description = "Logs the journal";
+    requires = [ "network-online.target" ];
+    path = with pkgs; [ jq zstd ];
+    # after = [ "systemd-journald.socket" ];
+    # FIXME still one final store before poweroff/reboot is needed
+    # this does not work
+    # before = [ "poweroff.target" "reboot.target" "halt.target" ];
+    # wantedBy = [ "reboot.target" "halt.target" "poweroff.target" ];
+
+    serviceConfig = {
+      ExecStart = lib.getExe pkg-journal-file-store;
+      WorkingDirectory = "/var/lib/journalgit/";
+      BindReadOnlyPaths = [
+        "/dev/log"
+        "/run/log/journal/" # non-persistent
+        "/var/log/journal/" # persistent
+      ];
+      Type = "oneshot";
+      ProtectHome = true;
+      PrivateTmp = true;
+      PrivateNetwork = true;
+      # DynamicUser = true;
+      # Group = "systemd-journal"; # to allow read-access
+    };
+  };
+  systemd.timers.journal-logger = {
+    wantedBy = [ "timers.target" ];
+    # execute every hour
+    timerConfig.OnCalendar = "*:00";
+    # and once after the unit starts
+    timerConfig.OnActiveSec = 0;
+  };
+}
