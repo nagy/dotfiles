603a876f10a07bba9f746b80ce8dcaff275aac95
Daniel Nagy <danielnagy@posteo.de>
2022-11-11 18:45:00 +0100

    Add unbound module

---
+++ modules/unbound.nix
@@ -0,0 +1,29 @@
+{ ... }:
+
+{
+  # Upstream DoH is not supported yet
+  # https://github.com/NLnetLabs/unbound/issues/308
+  services.unbound = {
+    enable = true;
+    resolveLocalQueries = false; # because we dont use 127.0.0.1
+    # man:unbound.conf
+    settings = {
+      # localControlSocketPath = "/run/unbound/unbound.ctl";
+      forward-zone = [{
+        name = ".";
+        forward-tls-upstream = true;
+        forward-addr =
+          [ "1.1.1.1@853#cloudflare-dns.com" "1.0.0.1@853#cloudflare-dns.com" ];
+      }];
+      server = {
+        interface = [ "0.0.0.0" ];
+        access-control = [ "0.0.0.0/0 allow" ];
+        log-queries = true;
+        log-replies = true;
+        hide-identity = true;
+        hide-version = true;
+        deny-any = true;
+      };
+    };
+  };
+}
