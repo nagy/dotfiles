4f09a6a41d31b8207859cbf5ac6109e32529fc24
Daniel Nagy <danielnagy@posteo.de>
2023-03-09 21:45:00 +0100

    bin/blocker: move to Hash struct

--- bin/blocker.rs
+++ bin/blocker.rs
@@ -8,6 +8,79 @@
 //! zstd = "0.12.3"
 //! ```
 
+struct Hash {
+    some: String,
+}
+
+impl Hash {
+    fn new(arg: &str) -> Self {
+        Self { some: arg.into() }
+    }
+    fn from_binary(bin: &[u8]) -> Self {
+        Self {
+            some: data_encoding::BASE32
+                .encode(bin)
+                .to_lowercase()
+                .replace("====", ""),
+        }
+    }
+    fn first(&self) -> String {
+        format!(
+            "{}{}",
+            self.some.chars().nth(0).unwrap(),
+            self.some.chars().nth(1).unwrap()
+        )
+    }
+    fn second(&self) -> String {
+        format!(
+            "{}{}",
+            self.some.chars().nth(2).unwrap(),
+            self.some.chars().nth(3).unwrap()
+        )
+    }
+    fn dirname(&self) -> String {
+        format!("{}/{}", self.first(), self.second())
+    }
+
+    fn try_filename(&self) -> String {
+        let paths = std::fs::read_dir(self.dirname()).unwrap();
+        let addr = paths
+            .filter(|p| {
+                p.as_ref()
+                    .unwrap()
+                    .file_name()
+                    .into_string()
+                    .unwrap()
+                    .starts_with(&self.some)
+            })
+            .next()
+            .unwrap()
+            .unwrap()
+            .file_name()
+            .into_string()
+            .unwrap();
+        format!("{}/{}/{}", self.first(), self.second(), addr)
+    }
+    fn read(&self) -> std::io::Result<Vec<u8>> {
+        let mut content = std::fs::read(self.try_filename())?;
+        // TODO check hash
+        mask(&mut content);
+        zstd::stream::decode_all(&content[..])
+    }
+}
+
+impl From<&str> for Hash {
+    fn from(source: &str) -> Self {
+        Self::new(source)
+    }
+}
+
+impl From<&[u8]> for Hash {
+    fn from(source: &[u8]) -> Self {
+        Self::from_binary(source)
+    }
+}
+
 fn mask(plaintext: &mut [u8]) {
     use aes::cipher::{KeyIvInit, StreamCipher};
     use hex_literal::hex;
@@ -21,58 +94,21 @@ fn mask(plaintext: &mut [u8]) {
     cipher.apply_keystream(plaintext);
 }
 
-fn dofile<T: std::io::Read + 'static>(input: T) -> impl Iterator<Item = std::io::Result<usize>> {
-    fastcdc::v2020::StreamCDC::new(Box::new(input), 512 << 10, 1024 << 10, 2048 << 10)
-        .map(|x| -> std::io::Result<usize> { Ok((x?).data.len()) })
-}
-
-fn read_onefile(addr: &str) -> std::io::Result<Vec<u8>> {
-    let first = format!(
-        "{}{}",
-        addr.chars().nth(0).unwrap(),
-        addr.chars().nth(1).unwrap()
-    );
-    let second = format!(
-        "{}{}",
-        addr.chars().nth(2).unwrap(),
-        addr.chars().nth(3).unwrap()
-    );
-    let dirname = format!("{}/{}", first, second,);
-    let paths = std::fs::read_dir(dirname)?;
-    let addr = paths
-        .filter(|p| {
-            p.as_ref()
-                .unwrap()
-                .file_name()
-                .into_string()
-                .unwrap()
-                .starts_with(addr)
-        })
-        .next()
-        .unwrap()
-        .unwrap()
-        .file_name()
-        .into_string()
-        .unwrap();
-    let filename = format!("{}/{}/{}", first, second, addr);
-    let mut ret = std::fs::read(filename)?;
-    mask(&mut ret);
-    zstd::stream::decode_all(&ret[..])
-}
+// fn dofile<T: std::io::Read + 'static>(input: T) -> impl Iterator<Item = std::io::Result<usize>> {
+//     fastcdc::v2020::StreamCDC::new(Box::new(input), 512 << 10, 1024 << 10, 2048 << 10)
+//         .map(|x| -> std::io::Result<usize> { Ok((x?).data.len()) })
+// }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     assert!(args.len() <= 2);
     if args.len() == 2 {
         // cat mode
-        let content = read_onefile(&args[1]).unwrap();
+        let h: Hash = args[1].as_str().into();
+        let content = h.read().unwrap();
         for ch in content.chunks_exact(32) {
-            let newname = data_encoding::BASE32
-                .encode(ch)
-                .to_lowercase()
-                .replace("====", "");
-            let innercontent = read_onefile(&newname).unwrap();
-            // TODO check hash
+            let chh: Hash = ch.into();
+            let innercontent = chh.read().unwrap();
             std::io::copy(&mut &innercontent[..], &mut std::io::stdout()).unwrap();
         }
     } else {
