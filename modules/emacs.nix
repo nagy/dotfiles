2644b00391953c2dd415deb391490635ee55c0ce
Daniel Nagy <danielnagy@posteo.de>
2023-06-26 00:45:00 +0200

    Reformat emacs module

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -6,65 +6,64 @@ let
     (lib.attrValues (import ../emacs {
       inherit pkgs lib;
       inherit (epkgs) emacs;
-    })) ++ (with epkgs;
-      with epkgs.melpaPackages; [
-        wat-mode
+    })) ++ (with epkgs; [
+      wat-mode
 
-        (sotlisp.overrideAttrs (_: {
-          src = pkgs.fetchFromGitHub {
-            owner = "nagy";
-            repo = "speed-of-thought-lisp";
-            rev = "bf1c906cfbd111d6d0218aa7b5f8f3d635d89083";
-            hash = "sha256-GsrIqnz+hPR1S0SkvduYp0rzmSHYkDDzFVWsLlgYEuM=";
-          };
-        }))
-        smart-mode-line
+      (sotlisp.overrideAttrs (_: {
+        src = pkgs.fetchFromGitHub {
+          owner = "nagy";
+          repo = "speed-of-thought-lisp";
+          rev = "bf1c906cfbd111d6d0218aa7b5f8f3d635d89083";
+          hash = "sha256-GsrIqnz+hPR1S0SkvduYp0rzmSHYkDDzFVWsLlgYEuM=";
+        };
+      }))
+      smart-mode-line
 
-        vterm
-        pdf-tools
-        org-pdftools
-        elfeed
-        triples
-        bufler
-        focus
-        osm
-        # devdocs
-        ts
-        lispy
-        lispyville
+      vterm
+      pdf-tools
+      org-pdftools
+      elfeed
+      triples
+      bufler
+      focus
+      osm
+      # devdocs
+      ts
+      lispy
+      lispyville
 
-        ov
-        visual-fill-column
+      ov
+      visual-fill-column
 
-        yaml-mode
-        csv-mode
+      yaml-mode
+      csv-mode
 
-        org
-        ascii-art-to-unicode
-        org-superstar
-        org-appear
-        org-ref
-        # org-fancy-priorities
-        nix-mode
-        mermaid-mode
-        ob-mermaid
-        # adaptive-wrap
-        lorem-ipsum
-        corfu
-        cape
-        jq-mode
-        rustic
-        format-all
+      org
+      ascii-art-to-unicode
+      org-superstar
+      org-appear
+      org-ref
+      # org-fancy-priorities
+      nix-mode
+      mermaid-mode
+      ob-mermaid
+      # adaptive-wrap
+      lorem-ipsum
+      corfu
+      cape
+      jq-mode
+      rustic
+      format-all
 
-        # password and secrets
-        pass
-        password-store
-        password-store-otp
+      # password and secrets
+      pass
+      password-store
+      password-store-otp
 
-        git-modes
-        tokei
-        dired-narrow
-      ]));
+      git-modes
+      tokei
+      dired-narrow
+    ]));
 in {
   environment.systemPackages = [
     emacsAndPackages
