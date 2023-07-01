a3f542a87cb4ae68a661a3229fb412859a9e145a
Daniel Nagy <danielnagy@posteo.de>
2023-07-01 15:00:00 +0200

    Add use-package keywords abbrev and cycle

--- emacs/nagy-use-package.el
+++ emacs/nagy-use-package.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords:
 ;; Homepage: https://github.com/nagy/nagy-use-package
-;; Package-Requires: ((emacs "29.1") ov)
+;; Package-Requires: ((emacs "29.1") ov dash)
 ;;
 ;; This file is NOT part of GNU Emacs.
 ;;
@@ -20,6 +20,7 @@
 ;;; Code:
 
 (require 'ov)
+(require 'dash)
 (require 'use-package)
 
 (defun use-package-normalize/:same (_name-symbol keyword args)
@@ -86,6 +87,43 @@
        `((add-hook ',(intern (concat mode-name "-hook")) #',(intern (concat "nagy-pretty-" mode-name "-h"))))
        `((setf (get ,mode 'nagy-pretty) ',(cdr rgx)))))))
 (add-to-list 'use-package-keywords :pretty t)
+;;; abbrev
+(defalias 'use-package-normalize/:abbrev 'use-package-normalize-forms)
+(defun use-package-handler/:abbrev (name-symbol _keyword rgx rest state)
+  (let* ((body (use-package-process-keywords name-symbol rest state))
+         (mode (car rgx))
+         (mode-name (symbol-name (cadr mode))))
+    (if (null rgx)
+        body
+      (use-package-concat
+       body
+       (cl-loop for x in (cdr rgx)
+                collect
+                `(define-abbrev ,(intern (concat mode-name "-abbrev-table")) ,(car x) ,(cdr x) nil :system t))
+       ))))
+(add-to-list 'use-package-keywords :abbrev t)
+;;; cycle
+;; TODO look at https://melpa.org/#/cycle-at-point
+(defvar nagy-cycle-alist nil)
+(defun nagy-cycle-dwim ()
+  (interactive)
+  (-let* (((beg . end) (bounds-of-thing-at-point 'symbol))
+          (sym (buffer-substring-no-properties beg end)))
+    (when (member sym (cl-loop for cell in nagy-cycle-alist
+                               collect (car cell)))
+      (delete-region beg end)
+      (insert (alist-get sym nagy-cycle-alist "" nil 'equal)))))
+(defalias 'use-package-normalize/:cycle 'use-package-normalize-forms)
+(defun use-package-handler/:cycle (name-symbol _keyword rgx rest state)
+  (use-package-concat
+   (use-package-process-keywords name-symbol rest state)
+   (cl-loop for x in (cdr rgx)
+            for rotated = (cons (car (last x)) (butlast x))
+            append
+            (cl-loop for zip in (-zip-pair x rotated)
+                     collect
+                     `(push ',zip nagy-cycle-alist)))))
+(add-to-list 'use-package-keywords :cycle t)
 
 (provide 'nagy-use-package)
 ;;; nagy-use-package.el ends here
