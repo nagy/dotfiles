7171c6ac9cb73ef3445cfc3c96f3c68967e64d86
Daniel Nagy <danielnagy@posteo.de>
2023-08-08 10:30:00 +0200

    Split htop module

--- modules/common.nix
+++ modules/common.nix
@@ -59,28 +59,6 @@
   # the starship binary could also be added to system packages. This is needed
   # when using prompt explanations
 
-  programs.htop = {
-    enable = true;
-    settings = {
-      delay = 2;
-      header_margin = 0;
-      hide_kernel_threads = 1;
-      hide_userland_threads = 1;
-      highlight_megabytes = 1;
-      left_meter_modes = [ 1 1 1 2 ];
-      left_meters = [ "AllCPUs2" "Memory" "Swap" "Zram" ];
-      right_meter_modes = [ 2 2 2 2 ];
-      right_meters = [ "Tasks" "LoadAverage" "Uptime" "Systemd" ];
-      show_program_path = 0;
-      tree_view = 1;
-      highlight_changes = 1;
-      highlight_changes_delay_secs = 2;
-      update_process_names = 1;
-      # manually removed "nice" and "prio" column
-      fields = "0 48 38 39 40 2 46 47 49 1";
-    };
-  };
-  environment.variables.HTOPRC = "/etc/htoprc";
   environment.localBinInPath = true;
   environment.homeBinInPath = true;
 
