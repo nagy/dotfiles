c22cfa06df85438b8cbc50ffdb948d18919241f8
Daniel Nagy <danielnagy@posteo.de>
2023-08-24 12:30:00 +0200

    Fix scanner with sane-backends patch

--- modules/scanner.nix
+++ modules/scanner.nix
@@ -3,4 +3,15 @@
 {
   hardware.sane.enable = true;
   users.users.user.extraGroups = [ "scanner" ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      # Fixes a problem that attempt to access /nix/store/.../var/lock .
+      # Without this, the scanner is not detected.
+      sane-backends = prev.sane-backends.overrideAttrs
+        ({ configureFlags ? [ ], ... }: {
+          configureFlags = configureFlags ++ [ "--disable-locking" ];
+        });
+    })
+  ];
 }
