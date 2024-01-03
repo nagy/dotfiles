d9cd789f7ce0213d4eb5d8ab8f91d463c662a810
Daniel Nagy <danielnagy@posteo.de>
2024-01-04 00:45:00 +0100

    Eglot refactor

--- emacs/nagy-lsp.el
+++ emacs/nagy-lsp.el
@@ -5,18 +5,18 @@
 (require 'general)
 
 (use-package eglot
-  :demand t
-  ;; :custom
-  ;; (eglot-autoshutdown t)
+  :defer t
+  :custom
+  ;; more tips
+  ;; https://old.reddit.com/r/emacs/comments/16vixg6/how_to_make_lsp_and_eglot_way_faster_like_neovim/
+  (eglot-events-buffer-size 0)
+  (eglot-sync-connect nil)
+  (eglot-send-changes-idle-time 0.1)
+  (eglot-autoshutdown t)
   :config
-  (setq eglot-send-changes-idle-time 0.1)
   ;; massive perf boost --- don't log every event
   (advice-add 'jsonrpc--log-event :override #'ignore)
   ;; or just (fset #'jsonrpc--log-event #'ignore)
-  ;; more tips
-  ;; https://old.reddit.com/r/emacs/comments/16vixg6/how_to_make_lsp_and_eglot_way_faster_like_neovim/
-  (setq eglot-sync-connect nil)
-  (setq eglot-events-buffer-size 0)
   :bind
   ("H-s-e" . eglot-rename)
   ("H-s-r" . eglot-inlay-hints-mode)
@@ -26,12 +26,19 @@
            "e" #'eglot
            "a" #'eglot-code-actions
            "q" #'eglot-code-action-quickfix
-           "s-k" #'eglot-shutdown-all)
+           "æ" #'eglot-code-action-quickfix
+           "s-k" #'eglot-shutdown-all
+           "⊢" #'eglot-format-buffer)
   (:states 'normal :keymaps 'eglot-mode-map
            "ſ" #'eglot-code-actions
            "M-æ" #'eglot-code-actions))
 
 ;; (use-package consult-eglot)
+
+(use-package flymake
+  :defer t
+  :custom
+  (flymake-show-diagnostics-at-end-of-line 'short))
 
 (provide 'nagy-lsp)
 ;;; nagy-lsp.el ends here
