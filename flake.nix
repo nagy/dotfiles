a400e4bf97e79c04a37148fb3e066bcdb855638e
Daniel Nagy <danielnagy@posteo.de>
2024-01-08 21:15:00 +0100

    Add haumea

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
+  inputs.haumea.url = "github:nix-community/haumea?ref=v0.2.2";
+  inputs.haumea.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, nur }:
+  outputs = { self, nixpkgs, nur, haumea }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -30,6 +32,7 @@
               ./hmmodule-zathura.nix
               ./hmmodule-readline.nix
             ];
+            nix.nixPath = [ "haumea=${haumea}" ];
           };
         };
       packages.${pkgs.system} = {
