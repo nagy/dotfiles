9e90828d3910c4cefebb2fe82a81181f5e18edc7
Daniel Nagy <danielnagy@posteo.de>
2024-01-29 23:45:00 +0100

    Update

--- emacs/nagy-vertico.el
+++ emacs/nagy-vertico.el
@@ -1,10 +1,14 @@
 ;;; nagy-vertico.el --- nagy-vertico config -*- lexical-binding: t; byte-compile-error-on-warn: t; -*-
 ;; Homepage: https://github.com/nagy/nagy
-;; Package-Requires: ((emacs "29.1") vertico consult marginalia orderless general nagy-use-package)
+;; Package-Requires: ((emacs "29.1") vertico embark consult consult-dir embark-consult marginalia orderless general nagy-evil nagy-use-package)
+
+(require 'nagy-evil)
 
 (require 'general)
 
 (require 'consult)
+(require 'embark)
+(require 'embark-consult)
 
 (use-package vertico
   :commands (vertico-mode)
@@ -13,7 +17,16 @@
   :custom
   (vertico-cycle nil)
   (vertico-scroll-margin most-positive-fixnum)
-  (vertico-count 7))
+  (vertico-count 7)
+  :bind
+  ("C-s--" . vertico-flat-mode)
+  (:map vertico-map
+        ("C-j"   . vertico-next)
+        ("C-M-j" . vertico-next-group)
+        ("C-k"   . vertico-previous)
+        ("C-M-k" . vertico-previous-group))
+  (:map minibuffer-mode-map
+        ("s--" . vertico-flat-mode)))
 
 (use-package vertico-quick
   :bind
@@ -29,15 +42,41 @@
            "°" #'vertico-buffer-mode))
 
 (use-package consult
+  :custom
+  (consult-async-min-input 2)
+  (consult-async-refresh-delay 0.15)
+  (consult-async-input-throttle 0.2)
+  (consult-async-input-debounce 0.1)
   :general
-  (:states 'normal "Ø" #'consult-outline)
+  (:states 'normal
+           "s" #'consult-line
+           "Ø" #'consult-outline)
   :bind
   ("s-b" . consult-buffer)
   ("s-/" . consult-focus-lines)
   ("H-/" . consult-keep-lines)
+  ("H-s" . consult-line)
+  ("C-s-s" . consult-ripgrep)
+  ([remap bookmark-jump] . consult-bookmar)
+  ([remap goto-line] . consult-goto-line)
+  ([remap imenu] . consult-imenu)
+  ([remap Info-search] . consult-info)
+  ([remap load-theme] . consult-theme)
+  ([remap man] . consult-man)
+  ([remap switch-to-buffer] . consult-buffer)
+  ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+  ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+  ([remap recentf-open-files] . consult-recent-file)
   :config
+  (keymap-global-set "<key-chord> ü j" #'consult-bookmark)
+  (keymap-global-set "<key-chord> ü b" #'consult-buffer)
   (consult-customize
-   consult-buffer :preview-key nil
+   consult-buffer
+   :preview-key nil
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file
+   consult--source-recent-file consult--source-project-recent-file consult--source-bookmark
+   :preview-key "C-SPC"
    ;; Disable preview for `consult-theme' completely.
    consult-theme :preview-key nil))
 
@@ -86,6 +125,13 @@
   :commands (marginalia-mode)
   :config
   (marginalia-mode))
+
+(use-package consult-dir
+  :bind
+  ([remap list-directory] . consult-dir)
+  (:map vertico-map
+        ("C-x C-d" . consult-dir)
+        ("C-x C-j" . consult-dir-jump-file)))
 
 (provide 'nagy-vertico)
 ;;; nagy-vertico.el ends here
