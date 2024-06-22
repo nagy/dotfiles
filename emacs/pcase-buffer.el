eb8bda342d32963b2d609108aaf6f6ea5db83abb
Daniel Nagy <danielnagy@posteo.de>
2024-06-22 22:00:00 +0200

    Add pcase-buffer.el

---
+++ emacs/pcase-buffer.el
@@ -0,0 +1,30 @@
+;;; pcase-buffer.el --- My local config -*- lexical-binding: t -*-
+
+(pcase-defmacro buffer (&rest fields)
+  (let ((pcase-buffer--expval (gensym "pcase-buffer--expval"))
+        (pcase-buffer-- (gensym "pcase-buffer--")))
+    `(and ,pcase-buffer--expval
+          (guard ,pcase-buffer--expval)
+          ,@(mapcar (lambda (field)
+                      (pcase-exhaustive field
+                        (`(,name ,pat)
+                         `(app (lambda (_arg)
+                                 (pcase ',name
+                                   ('point (with-current-buffer ,pcase-buffer--expval (point)))
+                                   ('size (with-current-buffer ,pcase-buffer--expval (buffer-size)))
+                                   ('hash (with-current-buffer ,pcase-buffer--expval (buffer-hash)))
+                                   ('process (get-buffer-process ,pcase-buffer--expval))))
+                               ,pat))
+                        ((pred symbolp)
+                         `(app (lambda (_arg)
+                                 (pcase ',field
+                                   ('point (with-current-buffer ,pcase-buffer--expval (point)))
+                                   ('size (with-current-buffer ,pcase-buffer--expval (buffer-size)))
+                                   ('hash (with-current-buffer ,pcase-buffer--expval (buffer-hash)))
+                                   ('process (get-buffer-process ,pcase-buffer--expval))))
+                               ,field))))
+                    fields))))
+
+
+(provide 'pcase-buffer)
+;;; pcase-buffer.el ends here
