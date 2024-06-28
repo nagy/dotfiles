db9c75c504c6fe5abcf438515b5dd7b93056fc12
Daniel Nagy <danielnagy@posteo.de>
2024-06-28 10:45:00 +0200

    Modus themes customizations

--- emacs/nagy-modus-themes.el
+++ emacs/nagy-modus-themes.el
@@ -31,7 +31,7 @@
   :preface
   (defun dayp ()
     "Return non-nil if it is day (aka light theme)."
-    (eq (modus-themes--current-theme) 'modus-operandi))
+    (eq (frame-parameter nil 'background-mode) 'light))
   :custom
   (modus-themes-bold-constructs t)
   (modus-themes-slanted-constructs t)   ; kursiv
@@ -53,26 +53,75 @@
      )))
 
 (use-package modus-themes
-  :functions nagy/modus-theme-overrides
-  :config
-  (defun nagy/modus-theme-overrides ()
+  :demand t
+  :preface
+  (defun nagy-modus-theme-overrides ()
     (interactive)
     (setenv "GTK_THEME" (if (dayp) "" "Adwaita:dark"))
     (set-face-attribute 'tab-bar-tab-inactive nil :box nil :background (face-attribute 'tab-bar :background nil t))
     (set-face-attribute 'window-divider nil :foreground (if (dayp) "black" "gray20"))
     (with-eval-after-load 'dired
       (set-face-attribute 'dired-header nil :font "Et Bembo" :height 2.0 :inherit 'modus-themes-heading-1)))
-  (add-hook 'modus-themes-after-load-theme-hook #'nagy/modus-theme-overrides))
+  (defun ala-fix-theme ()
+    (interactive)
+    (call-process (if (dayp) "ala-day" "ala-night")))
+  :config
+  (add-hook 'modus-themes-after-load-theme-hook #'nagy-modus-theme-overrides)
+  (add-hook 'modus-themes-after-load-theme-hook #'ala-fix-theme)
+  (load-theme 'modus-vivendi t))
+
+(use-package modus-themes
+  :preface
+  (defun my-modus-themes-custom-faces ()
+    (interactive)
+    (modus-themes-with-colors
+      (custom-set-faces
+       `(default ((,c :height 100)))
+       `(magit-section-heading ((,c :inherit modus-themes-heading-3)))
+       `(fixed-pitch ((,c :height unspecified)))
+       `(header-line ((,c :background unspecified)))
+       `(eros-result-overlay-face ((,c :background unspecified :box (:line-width -1 :color ,fg-main))))
+       `(modus-themes-lang-error ((,c :underline unspecified :inherit modus-themes-intense-red)))
+       `(modus-themes-lang-warning ((,c :underline unspecified :inherit modus-themes-intense-yellow)))
+       `(modus-themes-lang-note ((,c :underline unspecified :inherit modus-themes-intense-green)))
+       `(line-number ((,c :foreground unspecified :background unspecified :inherit (parenthesis default))))
+       `(line-number-current-line ((,c :foreground unspecified :background unspecified :inherit line-number)))
+       ;; `(nix-search-version ((,c :inherit modus-themes-fg-cyan)))
+       ;; `(tab-bar-tab-inactive ((,c :box nil :background ,bg-inactive)))
+       `(nix-search-version ((,c :inherit marginalia-version)))
+       `(nix-search-description ((,c :inherit marginalia-documentation)))
+       `(mode-line ((,c :box unspecified)))
+       `(mode-line-inactive ((,c :box unspecified)))
+       `(nameless-face ((,c :inherit font-lock-comment-delimiter-face)))
+       `(eglot-highlight-symbol-face ((,c :underline t :bold t)))
+       `(scroll-bar ((,c :box unspecified :background ,bg-main :foreground ,(if (dayp) "#ccc" "#333")))))))
+  (defun my-modus-themes-custom-faces-twice ()
+    "Call `my-modus-themes-custom-faces' twice because with only one
+call, the scrollbar on the second display is not updated
+correctly."
+    (interactive)
+    (my-modus-themes-custom-faces)
+    (my-modus-themes-custom-faces))
+  :config
+  (add-hook 'modus-themes-after-load-theme-hook #'my-modus-themes-custom-faces-twice)
+  ;; (add-hook 'modus-themes-after-load-theme-hook #'ala-fix-theme)
+  )
+
 
 (use-package paren-face
+  ;; :custom
+  ;; (paren-face-modes '(js-mode))
   :preface
   (defun nagy/fix-parenface ()
     (set-face-attribute 'parenthesis nil :foreground (if (dayp) "#ccc" "#333")))
   :config
   (setq paren-face-regexp "[][(){};,]")
   (global-paren-face-mode 1)
+  (nagy/fix-parenface)
   (push 'js-mode paren-face-modes)
+  (push 'js-json-mode paren-face-modes)
   (push 'c-mode paren-face-modes)
+  (push 'c++-mode paren-face-modes)
   (push 'nix-mode paren-face-modes)
   (push 'rustic-mode paren-face-modes)
   (push 'hy-mode paren-face-modes)
@@ -87,14 +136,19 @@
   (push 'sql-mode paren-face-modes)
   (push 'zig-mode paren-face-modes)
   (push 'java-mode paren-face-modes)
+  (push 'yaml-mode paren-face-modes)
+  (push 'hcl-mode paren-face-modes)
+  (push 'go-mode paren-face-modes)
+  (push 'go-dot-mod-mode paren-face-modes)
   (add-hook 'modus-themes-after-load-theme-hook #'nagy/fix-parenface))
 
 (use-package lin
   :functions lin-global-mode
+  ;; :custom
+  ;; (lin-mode-hooks '(elfeed-search-mode-hook))
   :config
   (setq lin-mode-hooks
-        '(dired-mode-hook
-          elfeed-search-mode-hook
+        '(elfeed-search-mode-hook
           ibuffer-mode-hook
           log-view-mode-hook
           magit-log-mode-hook
@@ -104,9 +158,11 @@
           nix-search-mode-hook
           proced-mode-hook
           tabulated-list-mode-hook))
-  (lin-global-mode 1))
+  (lin-global-mode 1)
+  (set-face-attribute 'hl-line nil :inherit 'lin-blue :background 'unspecified))
 
 (use-package nerd-icons
+  :demand t
   :bind
   ("C-H-s-e" . nerd-icons-insert))
 
