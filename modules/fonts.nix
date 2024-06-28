27d34af4c0b9d5fc1df6627c72b03253a9c56d5d
Daniel Nagy <danielnagy@posteo.de>
2024-06-28 11:00:00 +0200

    Remove iosevka custom build plan

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,15 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  # because line-height is too high with newest version
-  overrideBuildPlan = old: {
-    buildPlan = pkgs.writeText "private-build-plans.toml"
-      ((builtins.readFile old.buildPlan) + "\n" + ''
-        [buildPlans.iosevka-comfy.metric-override]
-        leading = 1100
-      '');
-  };
-in {
+{
   fonts = {
     fontconfig = {
       enable = config.services.xserver.enable;
@@ -27,15 +18,11 @@ in {
       includeUserConf = false;
     };
     enableDefaultPackages = true;
-    packages = with pkgs; [
-      # the override may not be needed if iosevka-comfy is not the main font
-      (iosevka-comfy.comfy.overrideAttrs overrideBuildPlan)
-      etBook # EtBembo https://edwardtufte.github.io/et-book/
-      noto-fonts
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-      # Only one icon used: 〜
-      # From font "file-icons". May be a bit overkill
-      # emacs-all-the-icons-fonts
+    packages = [
+      pkgs.iosevka-comfy.comfy
+      pkgs.etBook # EtBembo https://edwardtufte.github.io/et-book/
+      pkgs.noto-fonts
+      (pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
     ];
   };
 }
