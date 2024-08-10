7c62ce692acacb58e6959df507d1e825980cbc78
Daniel Nagy <danielnagy@posteo.de>
2024-08-10 12:15:00 +0200

    Move kubernetes into module

---
+++ modules/kubernetes.nix
@@ -0,0 +1,148 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+
+  imports = [ ./shortcommands.nix ];
+
+  environment.systemPackages = [
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+  ];
+
+  nagy.shortcommands = {
+    k = [ "kubectl" ];
+    kg = [
+      "kubectl"
+      "get"
+    ];
+    kgp = [
+      "kubectl"
+      "get"
+      "pod"
+    ];
+    kgd = [
+      "kubectl"
+      "get"
+      "deployment"
+    ];
+    kgn = [
+      "kubectl"
+      "get"
+      "node"
+    ];
+    kgpw = [
+      "kubectl"
+      "get"
+      "pod"
+      "--watch"
+    ];
+    kgdw = [
+      "kubectl"
+      "get"
+      "deployment"
+      "--watch"
+    ];
+    kgnw = [
+      "kubectl"
+      "get"
+      "node"
+      "--watch"
+    ];
+    kgsw = [
+      "kubectl"
+      "get"
+      "service"
+    ];
+    kd = [
+      "kubectl"
+      "describe"
+    ];
+    kdp = [
+      "kubectl"
+      "describe"
+      "pod"
+    ];
+    kdd = [
+      "kubectl"
+      "describe"
+      "deployment"
+    ];
+    kdn = [
+      "kubectl"
+      "describe"
+      "node"
+    ];
+    kc = [
+      "kubectl"
+      "create"
+    ];
+    kcp = [
+      "kubectl"
+      "create"
+      "pod"
+    ];
+    kcd = [
+      "kubectl"
+      "create"
+      "deployment"
+    ];
+    kcj = [
+      "kubectl"
+      "create"
+      "job"
+    ];
+    kcn = [
+      "kubectl"
+      "create"
+      "namespace"
+    ];
+    ke = [
+      "kubectl"
+      "exec"
+    ];
+    keti = [
+      "kubectl"
+      "exec"
+      "-it"
+    ];
+    kl = [
+      "kubectl"
+      "label"
+    ];
+    kw = [
+      "kubectl"
+      "wait"
+    ];
+    kr = [
+      "kubectl"
+      "run"
+    ];
+
+    A = [ "argocd" ];
+    Aa = [
+      "argocd"
+      "app"
+    ];
+    Aal = [
+      "argocd"
+      "app"
+      "list"
+    ];
+    Ac = [
+      "argocd"
+      "cluster"
+    ];
+    Acl = [
+      "argocd"
+      "cluster"
+      "list"
+    ];
+
+  };
+
+}
--- modules/shortcommands.nix
+++ modules/shortcommands.nix
@@ -76,38 +76,6 @@ let
     qh = [ "sqlite3" "-html" ];
     qc = [ "sqlite3" "-csv" ];
 
-    # # kubernetes
-    k = [ "kubectl" ];
-    kg = [ "kubectl" "get" ];
-    kgp = [ "kubectl" "get" "pod" ];
-    kgd = [ "kubectl" "get" "deployment" ];
-    kgn = [ "kubectl" "get" "node" ];
-    kgpw = [ "kubectl" "get" "pod" "--watch" ];
-    kgdw = [ "kubectl" "get" "deployment" "--watch" ];
-    kgnw = [ "kubectl" "get" "node" "--watch" ];
-    kgsw = [ "kubectl" "get" "service" ];
-    kd = [ "kubectl" "describe" ];
-    kdp = [ "kubectl" "describe" "pod" ];
-    kdd = [ "kubectl" "describe" "deployment" ];
-    kdn = [ "kubectl" "describe" "node" ];
-    kc = [ "kubectl" "create" ];
-    kcp = [ "kubectl" "create" "pod" ];
-    kcd = [ "kubectl" "create" "deployment" ];
-    kcj = [ "kubectl" "create" "job" ];
-    kcn = [ "kubectl" "create" "namespace" ];
-    ke = [ "kubectl" "exec" ];
-    keti = [ "kubectl" "exec" "-it" ];
-    kl = [ "kubectl" "label" ];
-    kw = [ "kubectl" "wait" ];
-    kr = [ "kubectl" "run" ];
-
-    # argocd
-    A = [ "argocd" ];
-    Aa = [ "argocd" "app" ];
-    Aal = [ "argocd" "app" "list" ];
-    Ac = [ "argocd" "cluster" ];
-    Acl = [ "argocd" "cluster" "list" ];
-
     # systemctl
     sc = [ "systemctl" ];
     scc = [ "systemctl" "cat" ];
