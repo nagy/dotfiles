8afd0deb1f60f4bfff262e36415f727005772331
Daniel Nagy <danielnagy@posteo.de>
2025-02-21 21:45:00 +0100

    Update starship

--- modules/starship.nix
+++ modules/starship.nix
@@ -1,34 +1,35 @@
-{ pkgs, lib, ... }:
-
 {
-  # simpler version of starship
-  # until https://github.com/starship/starship/issues/896 is fixed
-  # FIXME rework this for 24.05. presets are now part of the module
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.programs.starship;
+in
+{
+  # simpler version of starship until
+  # https://github.com/starship/starship/issues/896 is fixed
   environment.sessionVariables.STARSHIP_CONFIG =
-    let
-      mkDollarPrompt = lib.replaceStrings [ ">](bold green)" ] [ "\\\\$](bold green)" ];
-      mkGitBranch =
-        lib.replaceStrings
-          [ "[git_branch]\n" ]
-          [
-            "[git_branch]\nignore_branches = ['master', 'main']\n"
-          ];
-      basePreset = builtins.readFile "${pkgs.starship}/share/starship/presets/plain-text-symbols.toml";
-      basePresetModified =
-        ''
-          add_newline=false
-          [line_break]
-          disabled = true
-        ''
-        + (mkGitBranch (mkDollarPrompt basePreset));
-    in
-    toString (pkgs.writeText "starship-config.toml" basePresetModified);
+    (pkgs.writeText "starship-config.toml" (''
+      add_newline=false
+      [line_break]
+      disabled = true
+      ${lib.pipe "${cfg.package}/share/starship/presets/plain-text-symbols.toml" [
+        builtins.readFile
+        # mkDollarPrompt
+        (lib.replaceStrings [ ">](bold green)" ] [ "\\\\$](bold green)" ])
+        # mkGitBranch
+        (lib.replaceStrings [ "[git_branch]\n" ] [ "[git_branch]\nignore_branches = ['master', 'main']\n" ])
+      ]}
+    '')).outPath;
   programs.bash.interactiveShellInit = ''
     if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "29.1,eat") ]]; then
       if [[ -w /run/user/$UID ]] ; then
         export STARSHIP_CACHE=/run/user/$UID/starship-cache
       fi
-      eval "$(${pkgs.starship}/bin/starship init bash --print-full-init)"
+      eval "$(${cfg.package}/bin/starship init bash --print-full-init)"
     fi
     [ -n "$EAT_SHELL_INTEGRATION_DIR" ] && \
       source "$EAT_SHELL_INTEGRATION_DIR/bash"
@@ -39,7 +40,7 @@
     HISTSIZE = "1000000";
     HISTFILE = "$HOME/.local/share/bash_history";
   };
-  # the starship binary could also be added to system packages. This is needed
-  # when using prompt explanations and timings
-  environment.systemPackages = [ pkgs.starship ];
+  # the starship binary could also be added to system packages. This
+  # is needed when using prompt explanations and timings
+  environment.systemPackages = [ cfg.package ];
 }
