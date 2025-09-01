81a3f5aae554d72231854b470132e05e40307066
Daniel Nagy <danielnagy@posteo.de>
2025-09-01 23:00:00 +0200

    Move company config to nagy-misc2.el

--- emacs/nagy-misc2.el
+++ emacs/nagy-misc2.el
@@ -626,5 +626,37 @@ waits for input."
   ("H-M-U" . fennel-mode)
   )
 
+;; NIX-EMACS-PACKAGE: company
+(use-package company
+  :demand t
+  :diminish 'company-mode
+  :bind
+  ("C-M-s-¢" . company-mode)
+  (:map company-active-map
+        ("RET" . nil)
+        ("<return>" . nil)
+        ("C-l" . company-complete-selection)
+        ("<key-chord> f j" . company-complete-selection))
+  ;; (:map doom-leader-map
+  ;;       ("M-c" . company-mode))
+  ;; :config
+  ;; (map! :map company-active-map
+  ;;       "RET" nil
+  ;;       "<return>" nil)
+  :config
+  (global-company-mode)
+  :custom
+  (company-idle-delay 0.1)
+  (company-minimum-prefix-length 2)
+  ;; (company-tooltip-align-annotations t)
+  (company-tooltip-idle-delay 0.1)
+  (company-async-redisplay-delay 0.1)
+  (company-dabbrev-downcase nil)
+  ;; Only search the current buffer for `company-dabbrev' (a backend that
+  ;; suggests text your open buffers). This prevents Company from causing
+  ;; lag once you have a lot of buffers open.
+  (company-dabbrev-other-buffers nil)
+  )
+
 (provide 'nagy-misc2)
 ;;; nagy-misc.el ends here
